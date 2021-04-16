16d1de55b0e734dcb35617e3f6bd0af52e45c0f0
GMBAMorera <morera.georges@hotmail.fr>
2021-04-16 17:39:28 +0200

    some heroku twists

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Projet_8.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ocpurbeurre.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
