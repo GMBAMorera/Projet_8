3bb865f6e66f4e11dbdd629139ab5505dfb82dee
GMBAMorera <morera.georges@hotmail.fr>
2021-04-16 17:33:21 +0200

    some heroku twists

--- ocpurbeurre/wsgi.py
+++ ocpurbeurre/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Projet_8.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ocpurbeurre.settings')
 
 application = get_wsgi_application()
